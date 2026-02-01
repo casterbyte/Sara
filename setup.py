426e6d7bfc92345311db6375c9c4cd0a19738a02
Mahama Bazarov <mahamabazarov@mailbox.org>
2026-02-01 16:06:02 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sara",
-    version="1.3",
+    version="1.3.0",
     url="https://github.com/caster0x00/Sara",
     author="Mahama Bazarov",
     author_email="mahamabazarov@mailbox.org",
